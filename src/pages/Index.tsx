c2c84af0c08a93b7abbbed2f0c75c0177f40244e
KELVIN ATSU DJAYOURI <kad@KELVINs-MacBook-Pro.local>
2026-05-15 16:02:40 +0000

    feat: add experience section

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Skills from "@/components/Skills";
 import Education from "@/components/Education";
+import Experience from "@/components/Experience";
 import Projects from "@/components/Projects";
 import Services from "@/components/Services";
 import Contact from "@/components/Contact";
@@ -23,6 +24,7 @@ const Index = () => (
       <About />
       <Skills />
       <Education />
+      <Experience />
       <Projects />
       <Services />
       <Contact />
